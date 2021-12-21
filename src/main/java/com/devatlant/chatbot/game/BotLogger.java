6f897af9b3f914ac4bb6b5122b76a401aa510e1e
dariekrav <dariekrav@gmail.com>
2021-12-21 19:21:02 +0200

    fix log

--- src/main/java/com/devatlant/chatbot/game/BotLogger.java
+++ src/main/java/com/devatlant/chatbot/game/BotLogger.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 public class BotLogger {
 
     static void info(Logger logger, String message, User user){
-        logger.info("message [" + message + "] was sent with success for user [" + user.getFirstName() + " " + user.getLastName() + "]");
+        logger.info(message);
+        System.out.println("message [" + message + "] was sent with success for user [" + user.getFirstName() + " " + user.getLastName() + "]");
     }
 }
